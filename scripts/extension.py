add3fef687545bc1d19f4fad7a0ec696cfff7674
YazawaKenichi <tonfarken83825@gmail.com>
2024-03-28 02:58:26 +0900

    タブの追加

---
+++ scripts/extension.py
@@ -0,0 +1,27 @@
+import modules.scripts as scripts
+import gradio as gr
+import os
+
+from modules import script_callbacks
+
+# コンポーネントを作成
+def on_ui_tabs():
+    with gr.Blocks(analytics_enabled=False) as ui_component:
+        with gr.Row():
+            angle = gr.Slider(
+                minimum=0.0,
+                maximum=360.0,
+                step=1,
+                value=0,
+                label="Angle"
+            )
+            checkbox = gr.Checkbox(
+                False,
+                label="Checkbox"
+            )
+            # TODO: ここにコンポーネント/処理を足していく (cf. https://gradio.app/docs/#components)
+        return [(ui_component, "Send to LINE Notify", "extension_template_tab")]
+
+# 作成したコンポーネントをwebuiに登録
+script_callbacks.on_ui_tabs(on_ui_tabs)
+
